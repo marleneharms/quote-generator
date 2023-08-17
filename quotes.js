d88ef3667242645fd66e3e3ae31c7474cc67863a
“marleneharms” <marlene_harms@hotmail.com>
2023-08-17 00:58:38 -0600

    dynamic quote change

---
+++ quotes.js
@@ -0,0 +1,30 @@
+const localQuotes = [
+  {
+    text: "I am become death, the destroyer of worlds...",
+    author: null,
+  },
+  {
+    text: "The important thing is not to stop questioning. Curiosity has its own reason for existing.",
+    author: "Albert Einstein",
+  },
+  {
+    text: "In science, there are no shortcuts to truth.",
+    author: "Carl Sagan",
+  },
+  {
+    text: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    text: "The universe is under no obligation to make sense to you.",
+    author: "Neil deGrasse Tyson",
+  },
+  {
+    text: "The most beautiful thing we can experience is the mysterious. It is the source of all true art and science.",
+    author: "Albert Einstein",
+  },
+  {
+    text: "Somewhere, something incredible is waiting to be known.",
+    author: "Carl Sagan",
+  },
+];
--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("testing");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+// let apiQuotes = [];
+
+// Show new quotes
+function newQuote() {
+  // pick a random quote from apiQuotes array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown author";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API
+// async function getQuotes() {
+//   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+//   try {
+//     const response = await fetch(apiUrl); // Get Quotes
+//     apiQuotes = await response.json(); // JSON response
+//     newQuote();
+//   } catch (e) {
+//     alert(error);
+//   }
+// }
+
+// // On Load
+// getQuotes();
+
+newQuote();
